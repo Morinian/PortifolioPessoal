82bb03cb608354c83fb0504efbacf6292aedf8b9
Paula Martins <lea.paulinha@gmail.com>
2024-01-11 20:16:43 -0300

    ajuste responsivo primeira vez

---
+++ js/funcao.js
@@ -0,0 +1,13 @@
+function menuShow() {
+    let menuMobile = document.querySelector('.mobile-menu');
+    let menuMobileheader = document.querySelector('nav');
+    if (menuMobile.classList.contains('open')) {
+        menuMobileheader.classList.remove("fund");
+        menuMobile.classList.remove('open');
+        document.querySelector('.icon').src = "../img/menu_.svg";
+    } else {
+        menuMobile.classList.add('open');
+        menuMobileheader.classList.add("fund");
+        document.querySelector('.icon').src = "../img/close_.svg";
+    }
+}
